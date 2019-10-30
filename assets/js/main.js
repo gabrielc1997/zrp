aad2d0460ba2eac8b2ea1498eee6b4663f51f51b
Gabriel Carvalho <gabriel_1997@hotmail.com>
2019-10-30 12:22:04 -0300

    estrutura finalizada

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+const tilt = $('.tilt-image').tilt();
+
+
+
+
+
+
+
+
+
+
+$('.carousel').flickity({
+  // options
+  cellAlign: 'center',
+  contain: true,
+  pageDots: false,
+  autoPlay: true,
+  prevNextButtons: false,
+  draggable: false,
+  autoPlay: 2500
+});
+
+
